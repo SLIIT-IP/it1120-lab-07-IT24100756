06469ebd89fd98f49e700fbc22f51c14d4c38ada
IT24100756 <it24100756@my.sliit.lk>
2024-09-24 17:18:42 +0530

    Add files via upload

---
+++ Q2/IT24100756Lab7Q2A.java
@@ -0,0 +1,11 @@
+public class IT24100756Lab7Q2A {
+    public static void main(String[] args) {
+        // Print the dollar sign pattern
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print("$");
+            }
+            System.out.println();
+        }
+ }
+}
